2103709fe3cfcb82a1887e5b2f0950264cc5cec1
Harinimuthukumar05 <harinimuthukumar0@gmail.com>
2026-01-27 13:59:45 +0530

    Time: 1 ms (95.30%) | Memory: 85.3 MB (52.76%) - LeetSync

--- 932-monotonic-array/monotonic-array.java
+++ 932-monotonic-array/monotonic-array.java
@@ -5,6 +5,7 @@ class Solution {
         int n=nums.length;
         if(n==1)return true;
         for(int i=0;i<nums.length-1;i++){
+            if(!desc &&!asc)return false;
             if(nums[i]>nums[i+1])asc=false;
             if(nums[i]<nums[i+1])desc=false;
         }
