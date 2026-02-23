02fc7b4b91fb40b3710c39f185029a729eab28b4
Harini M <harinimuthukumar0@gmail.com>
2026-02-23 22:07:13 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (74.68%) - LeetSync

---
+++ 1137-height-checker/height-checker.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] count=new int[101];
+        for(int h:heights){
+            count[h]++;
+        }
+        int index=0;
+        int c=0;
+        for(int h=1;h<=100;h++){
+            while(count[h]>0){
+                if(heights[index]!=h){
+                    c++;
+                }
+                index++;
+                count[h]--;
+            }
+        }
+        return c;
+    }
+}
