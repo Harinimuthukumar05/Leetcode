30fa9f16428764651f53db01d73eb164cf2b1393
Harini M <harinimuthukumar0@gmail.com>
2026-08-14 15:46:28 +0530

    Time: 2 ms (41.34%) | Memory: 42.6 MB (95.83%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -2,7 +2,7 @@ class Solution {
     public int minAddToMakeValid(String s) {
         if(s.length()==0)return 0;
         Stack<Integer> st=new Stack<>();
-        int x=0;
+        int x=0;//invalid closing bracket tracking
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
             if(ch=='(')st.push(i);
@@ -13,6 +13,6 @@ class Solution {
                 }
             }
         }
-        return st.size()+x;
+        return st.size()+x;//ivalid open bracket inside stack+invalid closing bracket
     }
 }
